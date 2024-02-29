2349356bb4d82d4e2ed8b7ff0f88e9f715bcf4b0
davidmvalencia <david.mata.valencia@gmail.com>
2024-02-28 18:46:01 -0600

    Update

--- app.py
+++ app.py
@@ -5,20 +5,58 @@ import streamlit as st
 
 car_data = pd.read_csv('C:/Users/David/Vehicles-project/vehicles_us.csv')
 
-build_histogram = st.checkbox('Construir un histograma') # crea una casilla de verificación
+build_histogram = st.checkbox('Visualización con histogramas') # Crea una casilla de verificación
 
 if build_histogram:
-    st.write('Tipo de autos disponibles en venta')
-
-    fig = px.histogram(car_data, x="type")
+    st.write('Marcas y modelos de los autos disponibles con relación a su estado y kilometraje.')
+    # Areamos un histograma basado en el
+    fig = px.histogram(car_data, x="model", y="odometer", color="condition",
+                       color_discrete_map={"sedan": "blue", "suv": "green", "truck": "salmon"})
+    # Ajustar el tamaño del gráfico
+    fig.update_layout(
+        autosize=False,
+        width=800,  # Ajustando el ancho según tus preferencias
+        height=600)  # Ajustando la altura según tus preferencias
 
     st.plotly_chart(fig, use_container_width=True)
 
-build_scatt = st.checkbox('Construir diagrama de dispersion') # crea una casilla de verificacion
+if build_histogram:
+    st.write('Cilindros de motor con relación a su tipo de vehiculo y precio.')
+    # Creamos un histograma basado en el numero de cilindros de motor
+    fig = px.histogram(car_data, x="cylinders", y="price", color="type",
+                       color_discrete_map={"sedan": "blue", "suv": "green", "truck": "salmon"})
+    # Ajustar el tamaño del gráfico
+    fig.update_layout(
+        autosize=False,
+        width=800,  # Ajustando el ancho según tus preferencias
+        height=600)  # Ajustando la altura según tus preferencias
+
+    st.plotly_chart(fig, use_container_width=True)
+
+build_scatt = st.checkbox('Visualización con diagramas de dispersion') # crea una casilla de verificacion
 
 if build_scatt:
-    st.write('Precio de los autos acorde al modelo disponible')
+    st.write('Modelos por año de los vehiculos en relacion a su tipo y precio.')
+    # Creamos un gráfico de dispersión basado en el año del modelo 
+    fig = px.scatter(car_data, x="model_year", y="price", color="type",
+                     color_discrete_map={"sedan": "blue", "suv": "green", "truck": "black"})  
+    # Ajustar el tamaño del gráfico
+    fig.update_layout(
+        autosize=False,
+        width=800,  # Ajustando el ancho según tus preferencias
+        height=600)  # Ajustando la altura según tus preferencias
 
-    fig = px.scatter(car_data, x="model", y="price")
+    st.plotly_chart(fig, use_container_width=True)
 
+if build_scatt:
+    st.write('Kilometraje de los autos en relacion con su condicion y precio')
+    # creamos un gráfico de dispersión basado en el kilometraje
+    fig = px.scatter(car_data, x="odometer", y="price", color="condition",
+                     color_discrete_map={"good": "blue", "like new": "green", "fair": "red"})
+    # Ajustar el tamaño del gráfico
+    fig.update_layout(
+        autosize=False,
+        width=800,  # Ajustando el ancho según tus preferencias
+        height=600)  # Ajustando la altura según tus preferencias
+    
     st.plotly_chart(fig, use_container_width=True)
